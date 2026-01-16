1ec36411c09054247937d3d544219484c7e623cc
SumukhMore <sumukhmore@gmail.com>
2026-01-16 13:31:26 +0530

    Tuple

---
+++ tuple.py
@@ -0,0 +1,19 @@
+tup = ()
+print(tup)
+print(type(tup)) # This is an empty tuple
+
+tup1 = (1, 2, 3, 4, 5)
+print(tup1)
+print(type(tup1)) # This is a tuple
+
+tup2 = ("apple", "banana", "cherry")
+print(tup2)
+print(type(tup2)) # This is also a tuple
+
+tup3 = (1)
+print(tup3)
+print(type(tup3)) # This is not a tuple, it's an integer
+
+tup4 = (1,)
+print(tup4)
+print(type(tup4)) # This is a tuple with one element
