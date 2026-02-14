45b741514e379a0142b93bceafe152697db3eff6
SumukhMore <sumukhmore@gmail.com>
2026-02-14 18:00:13 +0530

    Add constructor example in OOP

--- oop-1/constructor.py
+++ oop-1/constructor.py
@@ -8,4 +8,4 @@ student1 = Student("Sumukh") # Create an instance of the Student class
 print(student1.name) 
 
 student2 = Student("Ananya") # Create another instance of the Student class
-print(student2.name)
+print(student2.name)# Constructor example in Python OOP
