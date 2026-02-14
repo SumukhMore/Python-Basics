0fe440fba65666357c1b32891961d00965e820a8
SumukhMore <sumukhmore@gmail.com>
2026-02-14 18:00:53 +0530

    Add delete object example in OOP

--- oop-2/delete.py
+++ oop-2/delete.py
@@ -6,4 +6,4 @@ s1 = Student("Sumukh")
 print(s1) # Output: <__main__.Student object at ...>
 print(s1.name)  # Output: Sumukh
 del s1 # Deleting the object reference
-print(s1)  # This will raise a NameError since s1 has been deleted
+print(s1)  # This will raise a NameError since s1 has been deleted# Delete object example in OOP
