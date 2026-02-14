0800e6cf06fcd022329a01ae1bce997d2408039d
SumukhMore <sumukhmore@gmail.com>
2026-02-14 17:49:12 +0530

    Add list slicing examples

--- data_structures/list_slicing.py
+++ data_structures/list_slicing.py
@@ -3,4 +3,4 @@ print(marks[1:4]) # Slicing from index 1 to 3
 print(marks[:3]) # Slicing from start to index 2 
 print(marks[2:]) # Slicing from index 2 to end 
 print(marks[-3:]) # Slicing the last three elements 
-print(marks[:-2]) # Slicing from start to the third last element
+print(marks[:-2]) # Slicing from start to the third last element# List slicing examples
