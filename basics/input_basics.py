693078ca4496f46e38280bb7fa4e04c1c0971ee7
SumukhMore <sumukhmore@gmail.com>
2026-02-14 14:14:36 +0530

    Add user input examples

--- basics/input_basics.py
+++ basics/input_basics.py
@@ -1,4 +1,4 @@
-#Taking input from user & printing it.
+#Taking input from user & printing it
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 price = float(input("Enter the price of the item: "))
