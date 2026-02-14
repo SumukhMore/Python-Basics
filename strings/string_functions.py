ee75c855c29ffe235b64c471de51204aa3a2fc2f
SumukhMore <sumukhmore@gmail.com>
2026-02-14 18:01:23 +0530

    Add string functions examples

--- strings/string_functions.py
+++ strings/string_functions.py
@@ -2,4 +2,4 @@ str = "i am a Python Developer"
 print(str.endswith("er")) #returns true value if the string ends with the specified value
 print(str.capitalize()) #converts the first character to upper case
 print(str.find("P")) #searches the string for a specified value and returns the position of where it was found
-print(str.count("a")) #returns the number of times a specified value occurs in a string
+print(str.count("a")) #returns the number of times a specified value occurs in a string# String functions examples
