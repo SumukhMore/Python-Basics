4453b3310ff680470de0c2ef41df0258b4c33100
SumukhMore <sumukhmore@gmail.com>
2026-02-14 17:59:38 +0530

    Add operators examples

--- math/operators.py
+++ math/operators.py
@@ -55,4 +55,4 @@ x = 10
 y = 20
 print(x > 5 and y > 15)  #Logical AND
 print(x > 5 or y < 15)   #Logical OR
-print(not(x > 5))        #Logical NOT
+print(not(x > 5))        #Logical NOT# Python operators examples
