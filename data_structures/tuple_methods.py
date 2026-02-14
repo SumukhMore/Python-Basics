a6341e82227df94fb8d9487b59815242fb115801
SumukhMore <sumukhmore@gmail.com>
2026-02-14 17:49:12 +0530

    Add tuple methods examples

--- data_structures/tuple_methods.py
+++ data_structures/tuple_methods.py
@@ -1,3 +1,3 @@
 tup = (2, 1, 3, 1)
 print(tup.index(1))  # Returns the index of the first occurrence of 1
-print(tup.count(1))  # Returns the number of times 1 appears in the tuple
+print(tup.count(1))  # Returns the number of times 1 appears in the tuple# Tuple methods examples
