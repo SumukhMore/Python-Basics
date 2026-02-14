06d561640ca2844c596469f25b5a08957799e887
SumukhMore <sumukhmore@gmail.com>
2026-02-14 18:52:47 +0530

    Add age calculator mini project

---
+++ mini_projects/age_calculator.py
@@ -0,0 +1,7 @@
+name = input("Enter you name:")
+birth_year = int(input("Enter your birth year:"))
+current_year = int(input("Enter the current year")) 
+age = current_year - birth_year
+
+print("Hello", name)
+print("Your age is", age)
