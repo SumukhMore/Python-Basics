4fb67672b711e2b611b68d30673aeeb64656c665
SumukhMore <sumukhmore@gmail.com>
2026-02-14 18:52:15 +0530

    Add simple profile

---
+++ mini_projects/simple_profile.py
@@ -0,0 +1,16 @@
+name = "Sumukh More"
+age = 20
+height = 6.0
+is_student = True
+
+# Print the profile information
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
+
+# Print the data types of each variable
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
